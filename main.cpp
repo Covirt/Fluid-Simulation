1442c9b65b275ea04ea6f6f05e5b9e91d33f1087
Covirt <vg.szwarcberg@gmail.com>
2022-08-22 17:06:15 +0100

    printer implemented

--- main.cpp
+++ main.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
+void printUniverse(const vector<vector<int>> vector2D){
+    for (const vector<int>& line : vector2D){
+        for (const int& box : line){
+            cout << box << ' ';
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     vector<vector<int>> universe(20,vector<int>(20,0));
 
-
+    printUniverse(universe);
 
     return 0;
 }
