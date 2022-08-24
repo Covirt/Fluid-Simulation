18faed1356f6c156ef16b63bd10e7985f5d6bb65
Covirt <vg.szwarcberg@gmail.com>
2022-08-24 11:45:29 +0100

    Implemnt averageNeighboors & vector<int> -> <dble>

--- main.cpp
+++ main.cpp
@@ -3,18 +3,34 @@
 
 using namespace std;
 
-void printUniverse(const vector<vector<int>> vector2D){
-    for (const vector<int>& line : vector2D){
-        for (const int& box : line){
+void printUniverse(const vector<vector<double>> vector2D){
+    for (const vector<double>& line : vector2D){
+        for (const double& box : line){
             cout << box << ' ';
         }
         cout << endl;
     }
 }
 
-void resetUniverse(vector<vector<int>>* p_vector2D){
-    for (vector<int>& line : *p_vector2D){
-        for (int& box : line){
+double averageNeighboors(vector<vector<double>> matrix){
+	int x = 1; int y = 1;
+	int sum = 0;
+	
+	for (int newY = y-1; newY <= y+1; newY++){
+		for (int newX = x-1; newX <= x+1; newX++){
+			sum += matrix[newY][newX];
+		}
+	}
+	
+	sum -= matrix[y][x];
+	double average = 1.0*sum/9;
+	
+	return average;
+}
+
+void resetUniverse(vector<vector<double>>* p_vector2D){
+    for (vector<double>& line : *p_vector2D){
+        for (double& box : line){
             box = 0;
         }
     }
@@ -23,7 +39,7 @@ void resetUniverse(vector<vector<int>>* p_vector2D){
 }
 
 
-void setUniverse(vector<vector<int>>* p_vector2D){
+void setUniverse(vector<vector<double>>* p_vector2D){
     int x; int y; int val;
 
     cout << "x coordinate you wish to set: ";
@@ -37,14 +53,40 @@ void setUniverse(vector<vector<int>>* p_vector2D){
 }
 
 
-void simulateUniverse(vector<vector<int>>* p_vector2D,const int numberOfIterations){
+void simulateUniverse(vector<vector<double>>* p_vector2D,const int numberOfIterations){
     cout << "You will be simulating the universe for " << numberOfIterations << " of iterations";
 }
+
+double averageNeighboors(vector<vector<double>> matrix, int x, int y){
+    int sum = 0; int numberOfItems = 0;
+    int yLOffset = 1; int yHOffset = 1;
+    int xLOffset = 1; int xHOffset = 1;
+
+    // the following if statements are made in order to prevent any seg faults
+    // when calculating the average at the borders
+    if (y == 0){yLOffset = 0;}
+    if (y == (matrix.size()-1)){yHOffset = 0;}
+    if (x == 0){xLOffset = 0;}
+    if (x == (matrix[1].size()-1)){xHOffset = 0;}
+
+    for (int newY = y-yLOffset; newY <= y+yHOffset; newY++){
+        for (int newX = x-xLOffset; newX <= x+xHOffset; newX++){
+            sum += matrix[newY][newX];
+            numberOfItems ++;
+        }
+    }
+
+    sum -= matrix[y][x]; numberOfItems--; // remove the center cell
+
+    return 1.0*sum/numberOfItems; // return average
+}
+
+
 
 int main(){
     string choice;
 
-    vector<vector<int>> universe(10,vector<int>(20,0));
+    vector<vector<double>> universe(10,vector<double>(20,0));
     printUniverse(universe);
 
     while (true){
